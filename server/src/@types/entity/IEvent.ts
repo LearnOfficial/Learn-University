b6e7a07528ecef9e9f946c4637b45721253bfb05
Camilo Andres Camargo Castañeda <camilocamargo49@gmail.com>
2023-02-26 17:02:57 -0500

    Event: Simple implementation

---
+++ server/src/@types/entity/IEvent.ts
@@ -0,0 +1,17 @@
+export enum EventTypeEnum{
+  "important"  = 1,
+  "normal" = 2,
+  "irrelevant" = 3,
+  "flexible" = 4
+}
+
+
+export type IEvent = { 
+  id?: number
+  title: string,
+  description: string, 
+  startDate: Date,
+  endDate: Date,
+  type: EventTypeEnum
+}
+
---
+++ server/src/entity/index.ts
@@ -0,0 +1,10 @@
+import Learner from "./Learner.js";
+import Events from "./Event.js";
+
+const entities: Array<any> = [
+  Learner,
+  Events
+]; 
+
+
+export {entities};
