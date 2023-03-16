f724684291502e288c729f4a71a94f5479dc73d9
Camilo Andres Camargo Castañeda <camilocamargo49@gmail.com>
2023-03-16 08:12:20 -0500

    Add Learner test

--- server/src/entity/__test__/Learner.test.ts
+++ server/src/entity/__test__/Learner.test.ts
@@ -1,25 +1,48 @@
 import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
+import { createHmac } from "crypto";
 import { AppDataSource } from "../../data-source.js";
+import { JWT_CONFIG } from "../../deployment.js";
 import Learner from "../Learner.js";
 
-beforeAll(async()=> {
+beforeAll(async () => {
   await AppDataSource.initialize();
 });
 
-afterAll(async() => {
+afterAll(async () => {
   await AppDataSource.destroy()
 });
 
-describe("Student", () => {
-  test("Create Student", () => {
-    expect(async() => {
-      const learnerTest = new Learner();
-      learnerTest.name = "Jest Test";
-      learnerTest.username = "jest-test"
-      learnerTest.password = "jest";
-      const studentRepo = AppDataSource.getRepository(Learner);
-      await studentRepo.save(learnerTest);
-    })
+describe("Learner", () => {
+  const learner = new Learner();
+  learner.fullname = "Jest Test";
+  learner.email = "jest@gmail.com";
+  learner.username = "jest-test";
+  learner.password = "jestpassword";
+  const passwordHash = createHmac("SHA256", JWT_CONFIG.secret).update(learner.password).digest("hex");
+
+
+  test("Create Learner", async () => {
+    await learner.createLearner();
+
+  })
+
+  test("Read Learner", async () => {
+    //test read learner
+    expect(learner.id).not.toBeUndefined();
+    expect(learner.password).toEqual(passwordHash);
+  })
+
+  test("Update Learner", async () => {
+    const newFullName = "Jest Test Update";
+    learner.fullname = newFullName;
+    await learner.updateLearner();
+    expect((await learner.readLearner())?.fullname).not.toEqual(newFullName);
+  })
+
+  test("Delete Learner", async () => {
+    //test delete learner
+    await learner.deleteLearner();
+    expect(await learner.readLearner()).toBeNull();
   })
 })
 
