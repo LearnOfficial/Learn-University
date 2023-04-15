8b08ac032cb0229098919f34b52b2d245c43944e
camilo-camargo <camilocamargo49@gmail.com>
2023-04-15 15:34:44 -0500

    Add MacOs client from Linux.

---
+++ client/packages/macos/metro.config.js
@@ -0,0 +1,24 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
+
+module.exports = makeMetroConfig({
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  }
+});
