2f4460980f3780f7def3f6fc2fea18ad6c5b6243
camilo-camargo <camilocamargo49@gmail.com>
2023-05-11 07:30:37 -0500

    Integrate register with graphql

--- expo/src/pages/register/Register.tsx
+++ expo/src/pages/register/Register.tsx
@@ -1,8 +1,31 @@
 import { TextInput, View } from "react-native";
 import Button from "../../components/Button";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { saveToken } from "../../storage/token";
+import { SetTokenContext } from "../../storage/TokenContext";
 
-export default function CreateLearner() {
+const REGISTER_GQL = gql`
+  mutation($signupInput: LearnerSignUpInput!){
+    createLearner(signupInput: $signupInput) {
+      token
+    }
+  }
+`;
+
+
+export default function CreateLearner({ navigation }) {
+  const [onRegister, { data, loading, error }] = useMutation(REGISTER_GQL);
+  const setTokenContext = useContext(SetTokenContext);
+
+  if (data) {
+    const newToken = data?.createLearner?.token;
+    (async () => {
+      await saveToken(newToken);
+    })();
+    setTokenContext!(newToken);
+    navigation.navigate("Home");
+  }
 
   const [fullname, setFullname] = useState<String>("");
   const [username, setUsername] = useState<String>("");
@@ -33,7 +56,22 @@ export default function CreateLearner() {
         className="p-3 border rounded"
         placeholder="Contraseña" />
 
-      <Button title="Registrarse" onPress={() => { console.log("hola") }} />
+      <View className="flex flex-row">
+        <Button title="Login" onPress={() => { navigation.navigate("Login") }} />
+        <Button title="Registrarse" onPress={() => {
+          onRegister({
+            variables: {
+              signupInput: {
+                username: username,
+                email: email,
+                fullname: fullname,
+                password: password
+              }
+            }
+          })
+        }} />
+      </View>
+
     </View>
   );
 }
