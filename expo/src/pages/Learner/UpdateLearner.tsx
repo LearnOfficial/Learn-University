bf69e68d5353b5707f9d4d38793c5a1e6e62f3f7
camilo-camargo <camilocamargo49@gmail.com>
2023-05-12 10:52:36 -0500

    Integrate update learner with GraphQL.

--- expo/src/pages/Learner/UpdateLearner.tsx
+++ expo/src/pages/Learner/UpdateLearner.tsx
@@ -1,39 +1,67 @@
 import { TextInput, View } from "react-native";
 import Button from "../../components/Button";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { saveToken } from "../../storage/token";
+import { SetTokenContext, TokenContext } from "../../storage/TokenContext";
 
-export default function UpdateLearner(){
+const UPDATE_LEARNER_GQL = gql`
+  mutation($learnerInput: LearnerUpdateInput!) {
+    updateLearner(learnerInput: $learnerInput) {
+      id
+    }
+  }
+`;
+
+
+export default function UpdateLearner({ navigation }) {
+  const token = useContext(TokenContext);
+  const context = { context: { headers: { Authorization: token } } };
+  const [onUpdateLearner, { data, loading, error }] = useMutation(UPDATE_LEARNER_GQL, {
+    ...context
+  });
+
+  if (data) {
+    navigation.navigate("Inicio");
+  }
+
+  if (error) {
+    alert("No se ha podido conectar a la Base de Datos.");
+  }
 
   const [fullname, setFullname] = useState<String>("");
-  const [username, setUsername] = useState<String>("");
-  const [email, setEmail] = useState<String>("");
   const [password, setPassword] = useState<String>("");
 
-  return(
+  return (
     <View className="flex flex-col w-screen h-screen justify-center items-center bg-white">
       <View className="flex flex-col">
-      <TextInput 
-      onChangeText={setFullname}
-      className="p-3 border rounded"
-      placeholder="Nombre Completo"/>
+        <TextInput
+          onChangeText={setFullname}
+          className="p-3 border rounded"
+          placeholder="Nombre Completo" />
 
-      <TextInput 
-      onChangeText={setUsername}
-      className="p-3 border rounded"
-      placeholder="Nombre de Usuario"/>
+        <TextInput
+          secureTextEntry
+          onChangeText={setPassword}
+          className="p-3 border rounded"
+          placeholder="Contraseña" />
 
-      <TextInput 
-      onChangeText={setEmail}
-      className="p-3 border rounded"
-      placeholder="Correo Electronico"/>
-
-      <TextInput 
-      secureTextEntry
-      onChangeText={setPassword}
-      className="p-3 border rounded"
-      placeholder="Contraseña"/>
-
-      <Button title="Actualizar Información" onPress={()=>{console.log("hola")}}/>
+        <View className="flex flex-row">
+          <Button title="Actualizar estudiante" onPress={() => {
+            if (password.length != 0) {
+              onUpdateLearner({
+                variables: {
+                  learnerInput: {
+                    fullname: fullname,
+                    password: password
+                  }
+                }
+              });
+            } else {
+              alert("Todos los campos son obligatorios.")
+            }
+          }} />
+        </View>
       </View>
     </View>
   );
