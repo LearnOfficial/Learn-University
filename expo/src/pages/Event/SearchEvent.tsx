b2fb55f430fa5d347000e46eda57ea0c034ebb94
camilo-camargo <camilocamargo49@gmail.com>
2023-05-11 23:20:12 -0500

    Integrate search events with GraphQL.

--- expo/src/pages/Event/SearchEvent.tsx
+++ expo/src/pages/Event/SearchEvent.tsx
@@ -1,29 +1,66 @@
-import {useState} from 'react';
-import { FontAwesome } from '@expo/vector-icons'; 
+import { useContext, useState } from 'react';
+import { FontAwesome } from '@expo/vector-icons';
 import { TouchableOpacity, TextInput, View, Text } from "react-native";
+import { TokenContext } from '../../storage/TokenContext';
+import { gql, useQuery } from '@apollo/client';
 
-export default function SearchEvent(){
+const USER_EVENTS_GQL = gql`
+  query{
+    learner {
+      events {
+        id
+        title
+        description
+        type
+        startDate
+        endDate
+      }
+    }
+  }
+`
 
-  const[title, setTitle] = useState<String>("")
+export default function SearchEvent() {
+  const token = useContext(TokenContext);
+  const { data, loading, error } = useQuery(USER_EVENTS_GQL, {
+    context: { headers: { Authorization: token } }
+  });
 
-  return(
-    <View className='flex flex-col p-3 w-screen h-screen justify-center items-center bg-white'>
-      <View className='flex flex-row w-3/6 '>
-        <TextInput 
-          className='p-3 border rounded m-2 grow'
-          onChangeText={setTitle}
-          placeholder="Titulo Evento"
-        />
+  const [title, setTitle] = useState<String>("")
 
-        <TouchableOpacity className='p-3' onPress={()=>console.log(title)}>
-          <FontAwesome name="search" size={26} color="black"/>
-        </TouchableOpacity>
+  if (data) {
+    const events = data?.learner?.events as [];
+    return (
+      <View className='flex flex-col p-3 w-screen h-screen justify-center items-center bg-white'>
+        <View className='flex flex-row w-3/6 '>
+          <TextInput
+            className='p-3 border rounded m-2 grow'
+            onChangeText={setTitle}
+            placeholder="Titulo Evento"
+          />
+
+          <TouchableOpacity className='p-3' onPress={() => console.log(title)}>
+            <FontAwesome name="search" size={26} color="black" />
+          </TouchableOpacity>
+        </View>
+
+        <View className='flex flex-col m-0.5 border rounded items-stretch'>
+          {
+            events.map((event: any, index) => {
+              return (
+                <View key={index} >
+                  <Text className="grow">{event.title}</Text>
+                </View>
+              )
+            })
+          }
+        </View>
       </View>
-    
-      <View className='border rounded w-3/6 h-3/6 justify-center'>
-        <View className='flex justify-center'>Evento 1</View>
-        <View className='flex justify-center'>Evento 2</View>
-      </View>
+    );
+  }
+
+  return (
+    <View className='flex w-screen h-screen justify-center items-center'>
+      <Text>No existen eventos</Text>
     </View>
-  );
+  )
 }
