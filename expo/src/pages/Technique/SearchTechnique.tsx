946296de91dfa7cef4ec37c1fb9a82be6e24c666
camilo-camargo <camilocamargo49@gmail.com>
2023-05-12 08:59:28 -0500

    Add delete technique with GraphQL.

--- expo/src/pages/Technique/SearchTechnique.tsx
+++ expo/src/pages/Technique/SearchTechnique.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
 import { useContext, useState } from "react";
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
@@ -18,18 +18,30 @@ query {
 }
 `
 
+const DELETE_TECHNIQUE_GQL = gql`
+  mutation($deleteTechniqueId: Float!){
+    deleteTechnique(id: $deleteTechniqueId)
+  }
+`
+
 export default function SearchTechnique() {
   const token = useContext(TokenContext);
-  
+  const context = { context: { headers: { Authorization: token } } }
+ 
   const { data, loading, error } = useQuery(USER_TECHNIQUES_GQL, {
-    context: { headers: { Authorization: token } }
+    ...context
+  }); 
+
+  const [onDeleteTechnique, techniqueDeleteMutation] = useMutation(DELETE_TECHNIQUE_GQL, {
+    ...context
   });
 
-  const [title, setTitle] = useState<String>("");
+  const [reload, setReload] = useState<boolean>(false);
+  const [title, setTitle] = useState<string>("");
 
   if (data) {
     const tecniques = data?.learner?.technique as [];
-    
+
     return (
       <View className="flex flex-col w-screen h-screen justify-center items-center bg-white">
         <View className="flex flex-col">
@@ -42,19 +54,26 @@ export default function SearchTechnique() {
           </View>
         </View>
         <View className='flex flex-col m-0.5 w-1/2 border rounded items-stretch'>
-            {
-              tecniques.map((technique:any, index)=>{
-                return(
-                  <View className='flex flex-row border rounded p-2 m-0.5 items-center' key={index}>
-                    <Text className='grow'> {technique.title} </Text>
-                    <TouchableOpacity onPress={()=>console.log(title)}>
-                      <AntDesign className="grow-0" name="delete" size={26} color="red"/>
-                    </TouchableOpacity>
-                  </View>
-                )
-              })
-            }
-          </View>
+          {
+            tecniques.map((technique: any, index) => {
+              return (
+                <View className='flex flex-row border rounded p-2 m-0.5 items-center' key={index}>
+                  <Text className='grow'> {technique.title} </Text>
+                  <TouchableOpacity onPress={() => {
+                    onDeleteTechnique({
+                      variables: {
+                        deleteTechniqueId: technique?.id
+                      }
+                    })
+                  }
+                  }>
+                    <AntDesign className="grow-0" name="delete" size={26} color="red" />
+                  </TouchableOpacity>
+                </View>
+              )
+            })
+          }
+        </View>
       </View>
     );
   }
